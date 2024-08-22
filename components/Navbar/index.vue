b585104ced60cc074bd26fe891a27060e2dff956
Yousef Roshandel <yousef.r1999@gmail.com>
2024-08-22 16:52:39 +0330

    implement Navbar menu

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -5,7 +5,7 @@
 			<!-- Left side -->
 			<div class="flex items-center text-gray-4">
 				<!-- Menu -->
-				<Icon name="ph:list-bold" class="text-3xl cursor-pointer hover:text-white" />
+				<Icon name="ph:list-bold" class="text-3xl cursor-pointer hover:text-white" @click="openMenu" />
 
 				<!-- Links -->
 				<div class="pl-12 space-x-7 hidden md:block">
@@ -30,14 +30,103 @@
 				<ButtonRoundedIconText text="Contact Us" icon="solar:call-chat-linear" link="/contact-us" />
 			</div>
 		</div>
+
+		<!-- Full-screen overlay menu -->
+		<div ref="menuOverlay" class="fixed inset-0 bg-black z-50 -translate-x-full">
+			<!-- Close Button -->
+			<div class="absolute top-4 right-4 z-50">
+				<Icon name="ph:x-bold" class="text-5xl text-gray-4 cursor-pointer hover:scale-x-105" @click="closeMenu" />
+			</div>
+
+			<!-- Menu Content -->
+			<div class="custom-wrapper relative h-screen w-full bg-black-3">
+				<!-- Items -->
+				<div
+					ref="navItems"
+					class="custom-container flex flex-col gap-8 items-start justify-center text-gray-4 font-thin text-6xl overflow-y-auto"
+				>
+					<NuxtLink to="/" class="nav-link" active-class="nav-active-link" @click="closeMenu">Home</NuxtLink>
+					<NuxtLink to="/about-us" class="nav-link" active-class="nav-active-link" @click="closeMenu"
+						>About Us</NuxtLink
+					>
+					<NuxtLink to="/services" class="nav-link" active-class="nav-active-link" @click="closeMenu"
+						>Services</NuxtLink
+					>
+					<NuxtLink to="/case-studies" class="nav-link" active-class="nav-active-link" @click="closeMenu"
+						>Case Studies</NuxtLink
+					>
+					<NuxtLink to="/contact-us" class="nav-link" active-class="nav-active-link" @click="closeMenu"
+						>Contact Us</NuxtLink
+					>
+				</div>
+				<!-- Social Media -->
+				<div ref="navSocial" class="absolute bottom-4 right-4 hidden md:block">
+					<WidgetSocialMedia />
+				</div>
+			</div>
+		</div>
 	</nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { $gsap } = useNuxtApp();
+
+const menuOverlay = ref(null);
+const navItems = ref<HTMLElement | null>(null);
+const navSocial = ref<HTMLElement | null>(null);
+
+const openMenu = () => {
+	menuOverlay.value.style.display = "block";
+
+	$gsap.fromTo(menuOverlay.value, { x: "-100%" }, { x: "0%", duration: 1, ease: "power4.out" });
+
+	if (navItems.value) {
+		$gsap.from(navItems.value.children, {
+			y: "-100",
+			stagger: 0.2,
+			duration: 0.75,
+			delay: 0.5,
+			opacity: 0,
+			ease: "power4.out",
+		});
+	}
+
+	if (navSocial.value) {
+		$gsap.from(navSocial.value, {
+			y: "200",
+			stagger: 0.2,
+			duration: 1,
+			delay: 1,
+			opacity: 0,
+			ease: "power4.out",
+		});
+	}
+};
+
+const closeMenu = () => {
+	$gsap.to(menuOverlay.value, {
+		x: "-100%",
+		duration: 1,
+		ease: "power4.in",
+		onComplete: () => {
+			menuOverlay.value.style.display = "none";
+		},
+	});
+};
+</script>
 
 <style scoped>
 .active-link button {
-	border-color: #4275f4;
-	color: white;
+	border-color: var(--primary);
+	color: var(--white);
+}
+
+.nav-link:hover {
+	transition: transform 0.5s ease;
+	transform: translateX(1rem) !important;
+}
+
+.nav-active-link {
+	color: var(--primary);
 }
 </style>
---
+++ components/widget/SocialMedia.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="flex flex-wrap items-center gap-6 text-gray-4 text-base font-bold">
+		<a
+			href="https://telegram.com/"
+			target="_blank"
+			class="nav-social flex items-center uppercase cursor-pointer gap-1.5"
+		>
+			<Icon name="ic:twotone-telegram" class="text-xl" />
+			telegram
+		</a>
+
+		<a
+			href="https://instagram.com/"
+			target="_blank"
+			class="nav-social flex items-center uppercase cursor-pointer gap-1.5"
+		>
+			<Icon name="mdi:instagram" class="text-xl" />
+			instagram
+		</a>
+
+		<a href="https://x.com/" target="_blank" class="nav-social flex items-center uppercase cursor-pointer gap-1.5">
+			<Icon name="hugeicons:new-twitter" class="text-xl" />
+			x
+		</a>
+
+		<a
+			href="https://facebook.com/"
+			target="_blank"
+			class="nav-social flex items-center uppercase cursor-pointer gap-1.5"
+		>
+			<Icon name="ic:sharp-facebook" class="text-xl" />
+			facebook
+		</a>
+
+		<a
+			href="https://linkedin.com/"
+			target="_blank"
+			class="nav-social flex items-center uppercase cursor-pointer gap-1.5"
+		>
+			<Icon name="mdi:linkedin" class="text-xl" />
+			linkedin
+		</a>
+	</div>
+</template>
+
+<style scoped>
+.nav-social:hover {
+	transition: transform 0.5s ease;
+	transform: translateY(0.25rem);
+}
+</style>
