2ed7abc7d3d8b958ee4fd831c0ff639e44c94a95
Yousef Roshandel <yousef.r1999@gmail.com>
2024-08-24 10:30:21 +0330

    implement Home page - about section

--- pages/index.vue
+++ pages/index.vue
@@ -11,31 +11,17 @@
 		<!-- Section 1: About -->
 		<section class="custom-wrapper py-[11.125rem] bg-black-3">
 			<div class="custom-container">
-				<h1 class="text-4xl font-bold mb-8">About</h1>
-				<p class="text-lg">
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, atque, quidem optio qui laudantium quo
-					officiis maiores exercitationem, cupiditate consequuntur nam! Perspiciatis dolores quas ipsam! Exercitationem
-					amet blanditiis suscipit fuga.
-				</p>
+				<h2 class="text-xl font-light tracking-widest text-gray-5">About</h2>
+				<h1 class="mt-6 stroked-text text-6xl font-bold uppercase">365<br />surveys</h1>
+				<div class="mt-6 flex flex-wrap gap-12 justify-between items-end">
+					<p class="text-gray-4 text-2xl font-normal w-2/3">
+						365 surveys is a Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+						ut labore et dolore magna aliqua.
+					</p>
+
+					<ButtonMoreRightArrow class="text-gray-4" text="Learn more about us" link="/about-us" />
+				</div>
 			</div>
-		</section>
-
-		<!-- Section 2: Services -->
-		<section class="custom-container py-16 bg-gray-800 text-white">
-			<h2 class="text-4xl font-bold mb-8">Our Services</h2>
-			<p class="text-lg leading-relaxed">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat elit. Phasellus egestas sapien et
-				ultricies dignissim. Sed sit amet metus ut leo fermentum tincidunt.
-			</p>
-		</section>
-
-		<!-- Section 3: Contact -->
-		<section class="custom-container py-16 bg-gray-700 text-white">
-			<h2 class="text-4xl font-bold mb-8">Contact Us</h2>
-			<p class="text-lg leading-relaxed">
-				If you have any questions, feel free to reach out to us at any time. We are here to help you with all your
-				needs.
-			</p>
 		</section>
 	</div>
 </template>
