62abc9c264bee8573607bd598faa0c9da5948c85
Yousef Roshandel <yousef.r1999@gmail.com>
2024-08-27 13:31:12 +0330

    https://learning.emofid.com/introduction-to-hami-fund/?utm_source=sms&utm_medium=text&utm_campaign=hami0306

---
+++ components/widget/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h1 class="text-white text-5xl font-bold">{{ animatedNumber }}<span class="text-primary">+</span></h1>
+		<span class="text-gray-4 text-lg font-thin">{{ title }}</span>
+	</div>
+</template>
+
+<script setup lang="ts">
+const { $gsap } = useNuxtApp();
+
+const props = defineProps<{
+	number: number;
+	title: string;
+}>();
+
+const animatedNumber = ref(0);
+
+onMounted(() => {
+	$gsap.to(animatedNumber, {
+		value: props.number,
+		duration: 1,
+		ease: "none",
+		onUpdate: () => {
+			animatedNumber.value = Math.floor(animatedNumber.value);
+		},
+	});
+});
+</script>
--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,21 +1,43 @@
 <template>
-	<div class="custom-wrapper h-screen pt-16 bg-black-3 text-white">
-		<div class="custom-container mt-16 space-y-8">
-			<h1>Page ABOUT US</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime illo perspiciatis, id a rerum distinctio sit
-				quos molestias dicta quis deleniti ad doloribus repudiandae suscipit, eveniet at vero soluta eius?
-			</p>
+	<div class="overflow-x-hidden bg-black-3">
+		<!-- Hero Section -->
+		<section class="custom-wrapper h-screen pt-32">
+			<div class="custom-container flex flex-col h-full">
+				<!-- Title -->
+				<div class="flex items-center text-gray-4 space-x-1.5 mb-8">
+					<icon class="text-2xl" name="tabler:info-octagon" />
+					<h1 class="text-xl font-bold">About Us</h1>
+				</div>
 
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime illo perspiciatis, id a rerum distinctio sit
-				quos molestias dicta quis deleniti ad doloribus repudiandae suscipit, eveniet at vero soluta eius?
-			</p>
+				<!-- Contents Wrapper -->
+				<div class="flex-grow flex items-center justify-center">
+					<div
+						class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-8 md:space-y-0 mb-[15%]"
+					>
+						<!-- Logo -->
+						<img src="/assets/images/Logo.svg" alt="Logo" class="h-[15rem]" id="logo" />
 
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime illo perspiciatis, id a rerum distinctio sit
-				quos molestias dicta quis deleniti ad doloribus repudiandae suscipit, eveniet at vero soluta eius?
-			</p>
-		</div>
+						<!-- Content -->
+						<div class="flex flex-col justify-between md:w-3/4 md:ml-8 h-[15rem]">
+							<p class="text-xl text-gray-4">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+								dolore magna aliqua.
+							</p>
+
+							<!-- Counters -->
+							<div class="flex flex-wrap gap-4 mt-8 md:mt-12 pb-12 md:pb-0">
+								<WidgetCounter number="10" title="Years Experiences" />
+								<WidgetCounter number="22" title="Happy Customers" />
+								<WidgetCounter number="06" title="Upcoming Projects" />
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Scroll -->
+				<WidgetScroll id="scroll" class="absolute hidden md:block bottom-12" />
+			</div>
+		</section>
 	</div>
 </template>
+
+<style scoped></style>
